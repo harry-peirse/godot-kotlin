d2d6a26a1f5707c458d0aa0d1076852e76eee5e9
smallporgies <peirse@gmail.com>
2019-02-17 22:09:21 +0100

    Failed Attempt at NativeScript.
    Until Kotlin Native supports passing structs as values in a callback,
    NativeScript will not be possible to achieve.
    
    This commit adds the code that attempts to do this, but it is commented
    out currently as it will not compile. Hopefully in future it will be
    supported and we can revisit.

--- godot-kotlin/src/mingwMain/kotlin/GodotKotlin.kt
+++ godot-kotlin/src/mingwMain/kotlin/GodotKotlin.kt
@@ -1,5 +1,6 @@
 import godotapi.*
 import kotlinx.cinterop.*
+//import platform.posix.strcpy
 
 class GDNative(val api: godot_gdnative_core_api_struct) {
     fun print(value: String) {
@@ -46,7 +47,7 @@ fun godot_nativescript_init(p_handle: COpaquePointer) {
 
     for (i in 0..gdNative!!.api.num_extensions.toInt()) {
         val extension = gdNative!!.api.extensions!![i]!!
-        if(extension[0].type == GDNATIVE_API_TYPES.GDNATIVE_EXT_NATIVESCRIPT.value) {
+        if (extension[0].type == GDNATIVE_API_TYPES.GDNATIVE_EXT_NATIVESCRIPT.value) {
             nativeScript = NativeScript(extension.reinterpret<godot_gdnative_ext_nativescript_api_struct>()[0])
             break
         }
@@ -54,4 +55,72 @@ fun godot_nativescript_init(p_handle: COpaquePointer) {
 
     gdNative?.printAPIVersion()
     nativeScript?.printAPIVersion()
+
+
+    /*
+    NATIVESCRIPT attempt 1
+
+    XXX: This doesn't work yet, cannot pass struct by value in callbacks, therefore registering types with NativeScript won't work
+
+    memScoped {
+
+        fun simple_constructor(instance: COpaquePointer?, method_data: COpaquePointer?): COpaquePointer? {
+            val user_data: COpaquePointer? = gdNative!!.api.godot_alloc!!("World from GDNative!".cstr.size)
+            val p: CPointer<ByteVar> = user_data!!.reinterpret()
+            strcpy(p, "World from GDNative!")
+
+            return user_data
+        }
+
+        fun create(): CValue<godot_instance_create_func> {
+            return cValue {
+                create_func = staticCFunction(::simple_constructor)
+            }
+        }
+
+        fun simple_destructor(instance: COpaquePointer?, method_data: COpaquePointer?, user_data: COpaquePointer?) {
+            gdNative!!.api.godot_free!!(user_data)
+        }
+
+        fun destroy(): CValue<godot_instance_destroy_func> {
+            return cValue {
+                destroy_func = staticCFunction(::simple_destructor)
+            }
+        }
+
+        fun getData(godot_object: COpaquePointer?,
+                    method_data: COpaquePointer?,
+                    user_data: COpaquePointer?,
+                    num_args: Int,
+                    args: CPointer<CPointerVar<godot_variant>>?
+        ): CValue<godot_variant> {
+            memScoped {
+                val data: CPointer<godot_string> = alloc<godot_string>().ptr
+                val ret: CValue<godot_variant> = cValue()
+
+                val userData: CPointer<ByteVar> = user_data!!.reinterpret()
+
+                gdNative!!.api.godot_string_new!!(data)
+                gdNative!!.api.godot_string_parse_utf8!!(data, userData)
+                gdNative!!.api.godot_variant_new_string!!(ret.ptr, data)
+                gdNative!!.api.godot_string_destroy!!(data)
+
+                return ret
+            }
+        }
+
+        nativeScript!!.api.godot_nativescript_register_class!!(p_handle, "SIMPLE".cstr.ptr, "Reference".cstr.ptr, create(), destroy())
+
+        val get_data: CValue<godot_instance_method> = cValue {
+            method = staticCFunction(::getData)
+
+        }
+
+        val attributes: CValue<godot_method_attributes> = cValue {
+            rpc_type = GODOT_METHOD_RPC_MODE_DISABLED
+        }
+
+        nativeScript!!.api.godot_nativescript_register_method!!(p_handle, "SIMPLE".cstr.ptr, "get_data".cstr.ptr, attributes, get_data)
+    }
+    */
 }
